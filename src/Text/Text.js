2db4aeb89bf14030699daed7805c6c1b76b4e5aa
raunak88 <raunak.kumar88@gmail.com>
2020-08-31 16:34:39 +0530

    Added header component

--- src/Text/Text.js
+++ src/Text/Text.js
@@ -1,19 +1,25 @@
-import React, { Component } from 'react';
-import TableAndForm from './TableAndForm';
-class Text extends Component{
-	render(){
-		return(<div className="tag0">
-				<h1>INVOICE</h1>
-				<hr id="hr1" />
-				<h2 className="tag1">My Company Name</h2>
-				<p className="tag2">Tata Consultancy Services<br/>
-				phone:9999999999<br/>
-				email:tcsglobal@tcs.com</p><br />
-				<br />
-				<br/>
-				<hr />				
-			</div>);
-	}
+import React, { Component } from "react";
+class Text extends Component {
+  render() {
+    return (
+      <div className="tag0">
+        <h1>INVOICE</h1>
+        <hr id="hr1" />
+        <h2 className="tag1">My Company Name</h2>
+        <p className="tag2">
+          My Services
+          <br />
+          phone:9999999999
+          <br />
+          email:myglobal@services.com
+        </p>
+        <br />
+        <br />
+        <br />
+        <hr />
+      </div>
+    );
+  }
 }
 
 export default Text;
